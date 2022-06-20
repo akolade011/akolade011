aa0409c5434cc8636976ec5ffb5fff018ddbd42a
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-06-21 00:08:04 +0100

    header updated

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import "~video-react/styles/scss/video-react"; // or import scss
+import ReactButton from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card';
+import Logo from '../../assets/images/bg.png'; 
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function Header ()  {
  const play = true;
@@ -15,13 +22,28 @@ function Header ()  {
         
 
 <nav>
-<header className='v-header containerH'>
-  <div className='fullscreen-video-wrap'>
-  <video src="video/3.mov" muted loop="true" autoPlay={play}></video>
-  </div>
 
-</header>
 </nav>
+
+<Container>
+  <Row>
+    <Col>
+    
+    </Col>
+  </Row>
+</Container>
+<Card className="bg-light text-white Header__Card" border="dark" style={{ width: '100%' }}>
+      <Card.Img src={Logo} alt="Card image" />
+      <Card.ImgOverlay>
+        <div className='Header__Card'>
+        <Card.Title>Real World Health</Card.Title>
+        <Card.Text>
+        We are at the heart of healthcare, data science and technology – providing advance data solutions to improve human health.
+        </Card.Text>
+        {/* <Card.Text>Last updated 3 mins ago</Card.Text> */}
+        </div>
+      </Card.ImgOverlay>
+    </Card>
                 </div>
 
             
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import {Auth0Provider} from '@auth0/auth0-react'
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const ClientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 
 
 ReactDOM.render(<Auth0Provider domain={domain} clientId ={ClientID} redirectUri={window.location.origin}><App /></Auth0Provider>, document.getElementById('root'));
